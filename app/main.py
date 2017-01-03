97eda7fb75e2447fe746f439a2c0f3191094f870
taniTk <tanitk91@gmail.com>
2017-01-03 21:54:48 +0900

    入力をパースする関数を追加

--- app/main.py
+++ app/main.py
@@ -1,4 +1,44 @@
 #!/usr/bin/env python
+
+import urllib.request, urllib.parse
+import json
+import asyncio
+import aiohttp
+
+
+end_point = "http://54.92.123.84/search?"
+api_key = "869388c0968ae503614699f99e09d960f9ad3e12"
+params = {
+    "q": "",
+    "wt": "json",
+    "ackey": api_key
+}
+
+
+def parse_args(argv):
+    keywords = argv[0][1:-1].replace('"', '').replace(', ', ',').split(',')
+    start_date = argv[1]
+    end_date = argv[2]
+    return {'keywords': keywords, 'start_date': start_date, 'end_date': end_date}
+
+
+def generate_url(keyword):
+    '''
+    keywordを含む記事を検索するAPIを生成
+    '''
+    params["q"] = "Body:" + str(keyword)
+    return end_point + urllib.parse.urlencode(params)
+
+
+async def get_response(keyword, url):
+    '''
+    keywordとそれに対応するAPIのURLを叩いて結果をいい感じのJSONにする
+    '''
+    response = await aiohttp.get(url)
+    data = await response.text()
+    json_data = json.loads(data)
+    return json_data
+
+
 def main(argv):
-  for v in argv:
-    print(v)
+    print(parse_args(argv))
