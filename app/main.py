dff7f64ce1cf73e1bf91e5f75a72bd8693794c07
taniTk <tanitk91@gmail.com>
2017-01-08 02:08:16 +0900

    求められる形式に出力するための処理を追加

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ import aiohttp
 import datetime
 import numpy as np
 import math
-
+import itertools
 
 end_point = "http://54.92.123.84/search?"
 api_key = "869388c0968ae503614699f99e09d960f9ad3e12"
@@ -50,22 +50,24 @@ def parse_response2week_num_list(res, start_date, end_date):
     '''
     # TODO
     # numFoundが100を超えていた場合の処理
+    if res['response']['result']['numFound'] == '0':
+        return init_week_num_lst(start_date, end_date)
+
     docs = res['response']['result']['doc']
-    print(res['response']['result']['numFound'])
     week_num_lst = init_week_num_lst(start_date, end_date)
     for doc in docs:
         release_date = str2date(doc['ReleaseDate'])
         idx = date2week_num_index(start_date, release_date)
-        print(str(release_date) + ' ' + str(idx))
-        week_num_lst[idx] += 1
+        if idx < len(week_num_lst):
+            week_num_lst[idx] += 1
     return week_num_lst
 
 
-async def url2week_num_dict(url, start_date_str, end_date_str):
+async def url2week_num_dict(keyword, url, start_date_str, end_date_str):
     res = await get_response(url)
     start_date = str2date(start_date_str)
     end_date = str2date(end_date_str)
-    return parse_response2week_num_list(res, start_date, end_date)
+    return {'keyword': keyword, 'doc_num_per_week': parse_response2week_num_list(res, start_date, end_date)}
 
 
 def str2date(date_str):
@@ -77,9 +79,9 @@ def str2date(date_str):
 
 def init_week_num_lst(start_date, end_date):
     '''
-    start_date ~ end_dateまでの週数個の要素を持つ0で初期化されたリスト(端数の日数も含む)
+    start_date ~ end_dateまでの週数個の要素を持つ0で初期化されたリスト(端数の日数は含まない)
     '''
-    lst_size = math.ceil((end_date - start_date).days / 7)
+    lst_size = math.floor((end_date - start_date).days / 7)
     return [0] * lst_size
 
 
@@ -91,6 +93,8 @@ def date2week_num_index(start_date, date):
 
 
 def pearson_correlation_coefficient(lst1, lst2):
+    if all([x == 0 for x in lst1]) or all([x == 0 for x in lst2]):
+        return None
     x = np.array(lst1)
     y = np.array(lst2)
     x_mean = np.mean(x)
@@ -102,23 +106,72 @@ def pearson_correlation_coefficient(lst1, lst2):
     return numerator / denominator
 
 
+def calc_all_combinations(keywords, results):
+    combinations = []
+    for comb in itertools.combinations(keywords, 2):
+        week_num_lsts = list(map(lambda x: x['doc_num_per_week'], filter(lambda r: r['keyword'] in comb, results)))
+        pcc = pearson_correlation_coefficient(*week_num_lsts)
+        if pcc != None:
+            pcc = float(str(round(pcc, 3)))
+        combinations.append({'comb': comb, 'correlation_coefficient': pcc})
+    return combinations
+
+
+def print_result(keywords, cc_result):
+    cc_mat = np.diag([1] * len(keywords)).tolist()
+    cc_result.reverse()
+
+    for i in range(0, len(keywords)):
+        for j in range(i+1, len(keywords)):
+            cc_mat[i][j] = cc_result.pop()['correlation_coefficient']
+
+    for i in range(0, len(keywords)):
+        for j in range(i+1, len(keywords)):
+            cc_mat[j][i] = cc_mat[i][j]
+
+    # print(json.dumps({"coefficients": cc_mat, "posChecker": True}))
+    print_for_test({"coefficients": cc_mat, "posChecker": True})
+
+
+def print_for_test(ans):
+    '''
+    ans(Dict型)をテストで求められる形式に変換
+    '''
+    coefficients = '['
+    for lst in ans['coefficients']:
+        lst_str = '['
+        for e in lst:
+            if e != None:
+                lst_str += str(e)
+            else:
+                lst_str += 'null'
+            lst_str += ','
+        lst_str = lst_str[:-1] + ']'
+        coefficients += lst_str + ','
+    coefficients = coefficients[:-1] + ']'
+
+    posChecker = 'true' if ans['posChecker'] == True else 'false'
+
+    print("{\"coefficients\":" + coefficients + ",\"posChecker\":" + posChecker + "}")
+
+
 def main(argv):
     args = parse_args(argv)
-    urls = []
+    keyword_and_urls = []
 
     for keyword in args['keywords']:
         url = generate_url(keyword, args['start_date'], args['end_date'])
-        urls.append(url)
+        keyword_and_urls.append({'keyword': keyword, 'url': url})
 
-    futures = [url2week_num_dict(url, args['start_date'], args['end_date']) for url in urls]
+    futures = [url2week_num_dict(k_and_u['keyword'], k_and_u['url'], args['start_date'], args['end_date']) for k_and_u in keyword_and_urls]
     loop = asyncio.get_event_loop()
     tasks = loop.run_until_complete(asyncio.wait(futures))[0]
 
-    results = [task.result()[:-1] for task in tasks]
-    for i in results:
-        print(i)
+    results = [task.result() for task in tasks]
+    cac = calc_all_combinations(args['keywords'], results)
+    print_result(args['keywords'], cac)
 
-    print(pearson_correlation_coefficient(results[0], results[1]))
+    # print(pearson_correlation_coefficient(results[0]['doc_num_per_week'], results[1]['doc_num_per_week']))
 
 
     # print(pearson_correlation_coefficient([2.8, 3.4, 3.6, 5.8, 7.0, 9.5, 10.2, 12.3, 13.2, 13.4],
